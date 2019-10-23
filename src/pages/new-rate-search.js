8fe333ea1da32bd74926cb8389e2abc960e794b3
Chetan Kokal <chetan.kokal@brainvire.com>
2019-10-23 18:59:12 +0530

    worked on rate-search and new-rate-search pages

--- src/pages/new-rate-search.js
+++ src/pages/new-rate-search.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "../styles/custom.css";
 import Headers from "../component/header";
 import SideMenu from "../component/sidemenu";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+
+const animatedComponents = makeAnimated();
 
 class NewRateSearch extends Component {
   constructor(props) {
@@ -11,6 +15,17 @@ class NewRateSearch extends Component {
   }
 
   render() {
+    const options = [
+      { value: "20 DC", label: "20 DC" },
+      { value: "30 DC", label: "30 DC" },
+      { value: "40 DC", label: "40 DC" },
+      { value: "50 DC", label: "50 DC" }
+    ];
+    const optionsSpeEqu = [
+      { value: "Refer Type", label: "Refer Type" },
+      { value: "abc", label: "abc" },
+      { value: "def", label: "def" }
+    ];
     return (
       <div>
         <Headers />
@@ -18,7 +33,7 @@ class NewRateSearch extends Component {
           <div className="cls-flside">
             <SideMenu />
           </div>
-          <div className="cls-rt text-center">
+          <div className="cls-rt">
             <div className="new-rate-cntr">
               <h3>Shipment Type</h3>
               <div className="new-radio-rate-cntr radio-blue">
@@ -86,8 +101,40 @@ class NewRateSearch extends Component {
               </div>
             </div>
             <div className="new-rate-cntr">
-              <h3>Dimensions</h3>
-              <div className="new-radio-rate-cntr radio-brown">
+              <h3>Equipment Types</h3>
+              <Select
+                className="rate-dropdown"
+                closeMenuOnSelect={false}
+                components={animatedComponents}
+                isMulti
+                options={options}
+              />
+              <div className="remember-forgot">
+                <input
+                  id="spe-equip"
+                  type="checkbox"
+                  name={"special equipment"}
+                />
+                <label htmlFor="spe-equip">Special Equipment</label>
+              </div>
+              <div className="spe-equ">
+                <label>Kind of Special Equipment</label>
+                <Select
+                  className="rate-dropdown"
+                  closeMenuOnSelect={false}
+                  components={animatedComponents}
+                  options={optionsSpeEqu}
+                />
+              </div>
+              <div className="spe-equ">
+                <label>Temperature of Special Equipment</label>
+                <input type="text" placeholder="Enter temperature in Celcius" />
+              </div>
+              <div className="remember-forgot">
+                <input id="haz-mat" type="checkbox" name={"haz-mat"} />
+                <label htmlFor="haz-mat">HazMat</label>
+              </div>
+              {/* <div className="new-radio-rate-cntr radio-brown">
                 <div>
                   <input type="checkbox" name="dimensions" id="dc-20" />
                   <label htmlFor="dc-20">20 DC</label>
@@ -109,11 +156,46 @@ class NewRateSearch extends Component {
                   <input type="checkbox" name="dimensions" id="dc-50" />
                   <label htmlFor="dc-50">50 DC</label>
                 </div>
+              </div> */}
+            </div>
+            <div className="new-rate-cntr">
+              <h3>Inco Terms</h3>
+              <div className="spe-equ">
+                <label>Inco Terms</label>
+                <input type="text" placeholder="Inco Terms" />
               </div>
             </div>
-            <a href="rate-table" className="butn blue-butn rate-search">
-              Search
-            </a>
+            <div className="new-rate-cntr">
+              <h3>Type of Move</h3>
+              <div className="new-radio-rate-cntr radio-blue">
+                <div>
+                  <input type="radio" name="type-move" id="p2p" />
+                  <label htmlFor="p2p">Port2Port</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="type-move"
+                    id="d2p"
+                    defaultChecked
+                  />
+                  <label htmlFor="d2p">Door2Port</label>
+                </div>
+                <div>
+                  <input type="radio" name="type-move" id="d2d" />
+                  <label htmlFor="d2d">Door2Door</label>
+                </div>
+                <div>
+                  <input type="radio" name="type-move" id="p2d" />
+                  <label htmlFor="p2d">Port2Door</label>
+                </div>
+              </div>
+            </div>
+            <div className="text-center">
+              <a href="rate-table" className="butn blue-butn rate-search">
+                Search
+              </a>
+            </div>
           </div>
         </div>
       </div>
