d6d249d343158a157b4772d8c33ac90b414c1029
Rashmi Salaria <rashmi.salaria@brainvire.com>
2019-12-13 14:58:29 +0530

    Response Change

--- src/pages/new-rate-search.js
+++ src/pages/new-rate-search.js
@@ -143,10 +143,11 @@ class NewRateSearch extends Component {
           SpecialContainerCode: "",
           PackageType: "",
           Quantity: 0,
-          Lengths: 0,
-          Width: 0,
-          Height: 0,
-          Weight: 0,
+          length: 0,
+          width: 0,
+          height: 0,
+          Gross_Weight: 0,
+          total:0,
           VolumeWeight: 0,
           Volume: 0
         }
@@ -1345,21 +1346,22 @@ class NewRateSearch extends Component {
           ...prevState.flattack_openTop,
           {
             SpecialContainerCode: optionsVal[0].SpecialContainerCode,
+            // PackageType: "",
+            // Quantity: 0,
+            // Lengths: 0,
+            // Width: 0,
+            // Height: 0,
+            // Weight: 0,
+
             PackageType: "",
-            Quantity: 0,
-            Lengths: 0,
-            Width: 0,
-            Height: 0,
-            Weight: 0,
+            length: "",
+            width: "",
+            height: "",
+            Quantity: "1",
+            Gross_Weight: "",
+            total: "",
             VolumeWeight: 0,
             Volume: 0
-            // PackageType: "",
-            // length: "",
-            // width: "",
-            // height: "",
-            // Quantity: "1",
-            // Gross_Weight: "",
-            // total: ""
           }
         ]
       }));
@@ -2037,7 +2039,20 @@ class NewRateSearch extends Component {
         ],
         users: [],
         referType: [],
-        flattack_openTop: [],
+        flattack_openTop: [
+          {
+            SpecialContainerCode: "",
+            PackageType: "",
+            Quantity: 0,
+            length: 0,
+            width: 0,
+            height: 0,
+            Gross_Weight: 0,
+            total:0,
+            VolumeWeight: 0,
+            Volume: 0
+          }
+        ],
         spacEqmtType: [],
 
         TruckTypeData: [
